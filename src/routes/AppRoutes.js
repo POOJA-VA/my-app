3cb8a247caa5b4231cfbaaca19f2400832e64cd8
Pooja Venakatesan <pooja.venkatesan@kumaran.com>
2026-05-27 18:06:38 +0530

    Initial project setup

--- src/routes/AppRoutes.js
+++ src/routes/AppRoutes.js
@@ -1,16 +1,12 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Dashboard from "../pages/Dashboard";
 import Employees from "../pages/Employees";
-import Invoice from "../pages/Invoice/Invoice";
 import Reports from "../pages/Reports";
 import Tasks from "../pages/Tasks";
 import Settings from "../pages/Settings";
 
 import Projects from "../pages/Projects";
 import Layout from "../layouts/Layout";
-import InvoiceMaster from "../pages/Invoice/InvoiceMaster";
-import InvoiceMailReports from "../pages/Invoice/InvoiceMailReports";
-import InvoiceMail from "../pages/Invoice/InvoiceMail";
 
 const AppRoutes = () => {
   return (
